42d951e83ebee3ba3add37e0370aa9ea889d5924
Cezar Nogueira <cezartv1221@gmail.com>
2024-10-21 20:32:42 -0300

    fix: alteração no app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import './App.css';
 
 export default function App() {
-export default function App() {
 
   return (
     <div className='app-container'>
     </div>
   );
 }
-
-export default App
