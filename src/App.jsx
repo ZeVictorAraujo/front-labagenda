093e9acc0d16e40ac8c5ebe2f50f83b88de1d659
Cezar Nogueira <cezartv1221@gmail.com>
2024-10-22 20:37:46 -0300

    Fix: Finalizado SideBar - Mudança de permissões.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
+import { useState } from 'react';
 import Sidebar from './components/SideBar/SideBar.jsx';
 
 export default function App() {
 
+  const [userType, setUserType] = useState('admin');
   return (
     
     <div>
-      <Sidebar />
+      <Sidebar userType={userType}/>
     </div>
 
   );
