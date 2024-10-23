ef3f6ce5f58d87b576cae8b5bea2aa5111ecc7a2
Cezar Nogueira <cezartv1221@gmail.com>
2024-10-23 20:37:04 -0300

    Fix: Ajeitar o app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Sidebar from './components/SideBar/SideBar'
 import FullCalendar from './components/FullCalendar/FullCalendar'
 
-function App() {
+export default function App() {
   return (
     
       <h1 className='text-2xl font-bold underline'>Olá</h1>
