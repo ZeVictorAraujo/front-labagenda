6e5a2d4e9ad8f51b397a8ec5b9251d1b728e1ede
ZeVictorAraujo <josearaujo.ba@hotmail.com>
2024-10-23 00:21:08 -0300

    Fix: Separação entre SideBar e Dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import './App.css';
-import { useState } from 'react';
-import Sidebar from './components/SideBar/SideBar.jsx';
-import Banner from './components/Banner/Banner.jsx'
+import Sidebar from './components/SideBar/SideBar'
+import Banner from './components/Banner/Banner'
 
-export default function App() {
-
-  const [userType, setUserType] = useState('admin');
+function App() {
   return (
-    
-    <div>
-      <Sidebar userType={userType}/>
-    </div>
+    <div className="flex h-screen">
+      {/* Sidebar Fixa à Esquerda */}
+      <Sidebar />
 
+      {/* Conteúdo Principal à Direita */}
+      <div className="flex-1 bg-gray-100 p-6 overflow-auto">
+        < Banner />
+      </div>
+    </div>
   );
 }
+
+export default App;
