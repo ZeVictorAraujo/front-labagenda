100c823d9c514885a8a9b978175be1d7613a9533
Cezar Nogueira <cezartv1221@gmail.com>
2024-10-24 11:14:56 -0300

    Fix: Adicionar Animações ao botão

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -3,12 +3,12 @@ import { tv } from 'tailwind-variants';
 export default function Button({ children, size = 'normal', color = 'primary' }) {
 
     const button = tv({
-        base: 'font-medium bg-blue-700 text-white rounded-md active:opacity-80',
+        base: 'font-medium bg-blue-700 text-white rounded active:opacity-80',
         variants: {
           color: {
-            primary: 'bg-blue-700 text-white',
-            edit: 'bg-yellow-500 text-white',
-            delete: 'bg-red-500 text-white'
+            primary: 'bg-blue-700 text-white hover:bg-blue-800 duration-100',
+            edit: 'bg-yellow-500 text-white hover:bg-yellow-600 duration-100',
+            delete: 'bg-red-500 text-white hover:bg-red-600 duration-100'
           },
           size: {
             normal: 'px-3 py-2',
