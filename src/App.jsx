8778a7c079cdba49aa9a93d61be8995292a6334a
Cezar Nogueira <cezartv1221@gmail.com>
2024-10-23 21:11:22 -0300

    Fix: Retornar o padrao do app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import Sidebar from './components/SideBar/SideBar'
-import FullCalendar from './components/FullCalendar/FullCalendar'
 
 export default function App() {
   return (
-    
-      <h1 className='text-2xl font-bold underline'>Olá</h1>
+    <div className="flex h-screen">
+      {/* Sidebar Fixa à Esquerda */}
+      <Sidebar />
+
+      {/* Conteúdo Principal à Direita */}
+      <div className="flex-1 bg-gray-100 p-6 overflow-auto">
+      </div>
+    </div>
   );
 }
