0edfc5fb445b4d7dc8ee5ceea47c1ff1bd602439
Cezar Nogueira <cezartv1221@gmail.com>
2024-10-28 10:30:09 -0300

    Fix: Adicionar Cor secundaria no botão e sombras

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -6,9 +6,10 @@ export default function Button({ children, size = 'normal', color = 'primary' })
         base: 'font-medium bg-blue-700 text-white rounded active:opacity-80',
         variants: {
           color: {
-            primary: 'bg-blue-700 text-white hover:bg-blue-800 duration-100',
-            edit: 'bg-yellow-500 text-white hover:bg-yellow-600 duration-100',
-            delete: 'bg-red-500 text-white hover:bg-red-600 duration-100'
+            primary: 'bg-blue-700 text-white hover:bg-blue-800 duration-100 shadow-sm',
+            secondary: 'bg-white text-neutral-950 hover:bg-slate-200 duration-100 shadow-sm',
+            edit: 'bg-yellow-500 text-white hover:bg-yellow-600 duration-100 shadow-sm',
+            delete: 'bg-red-500 text-white hover:bg-red-600 duration-100 shadow-sm'
           },
           size: {
             normal: 'px-3 py-2',
