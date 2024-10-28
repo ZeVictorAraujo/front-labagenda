1022b966f40b9adfc934f7f11d77352b5b262f3a
Cezar Nogueira <cezartv1221@gmail.com>
2024-10-28 11:47:54 -0300

    Fix: Adicionar Integração da Search Bar com o Backend

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,7 +1,34 @@
-import Input from "../Input/Input.jsx";
+import React, { useState, useEffect } from "react";
 import { FaSearch } from "react-icons/fa";
 
 export default function Searchbar() {
+
+    const [searchTerm, setSearchTerm] = useState("");
+    const [results, setResults] = useState([]);
+
+    useEffect(() => {
+        const fetchResults = async () => {
+            if (searchTerm.length >= 3) {
+                try {
+                const response = await fetch(`/api/search?query=${searchTerm}`);
+                const data = await response.json();
+                setResults(data);
+                } catch (error) {
+                console.error("Erro ao buscar dados:", error);
+                }
+            }
+        };
+
+        // Debounce(Limitar a quantidade de vezes que a função é executada) de 500ms antes de fazer a busca
+        const debounceTimeout = setTimeout(fetchResults, 500);
+
+        // Limpar o timeout caso o usuário continue digitando
+        return () => clearTimeout(debounceTimeout);
+    }, [searchTerm]);
+
+    const handleChange = (e) => {
+        setSearchTerm(e.target.value);
+    };
 
     return (
         <div className="flex w-96 rounded bg-white">
@@ -10,11 +37,23 @@ export default function Searchbar() {
             name="search"
             className="w-full px-4 py-2 border-none outline-none focus:outline-none" 
             size="normal" 
-            type="search" 
+            type="search"
+            value={searchTerm}
+            onChange={handleChange}
             ph="Pesquisar" />
+
             <button className="m-2 px-2 py-2 hover:text-blue-800 duration-100">
               <FaSearch />
             </button>
+
+            {/* Lista de resultados */}
+            <ul className="absolute bg-white mt-1 w-96 rounded shadow-lg">
+                {results.map((result) => (
+                    <li key={result.id} className="p-2 hover:bg-gray-100">
+                        {result.name}
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 }
