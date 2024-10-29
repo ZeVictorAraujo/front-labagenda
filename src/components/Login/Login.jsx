f03abc217eed6ab21558f1d435670be5784b2887
Cezar Nogueira <cezartv1221@gmail.com>
2024-10-29 09:26:42 -0300

    Fix: Mudar para Formulario de Login(POST) e ajustar Logo do Login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,12 +1,25 @@
 import Card from "../Card/Card";
 import Logo from "../Logo/Logo";
+import Input from "../Input/Input";
+import Button from "../Button/Button";
 
 export default function Login() {
     return (
-        <div>
             <Card size="normal" color="secondary" width="sm">
-                <Logo />
+                <div className="flex flex-col itens-center justify-center gap-4">
+                    {/* Div da Logo */}
+                    <div className="flex itens-center justify-center">
+                        <div className="flex itens-center justify-center w-3/4">
+                            <Logo type="secondary"/>
+                        </div>
+                    </div>
+                    {/* Formulario de Login */}
+                    <form action="POST" className="flex flex-col itens-center justify-center gap-4">
+                        <Input size="normal" type="text" ph="RA" id="ra" name="ra"/>
+                        <Input size="normal" type="text" ph="Senha" id="senha" name="senha"/>
+                        <Button size="normal" color="primary" type="text">Entrar</Button>
+                    </form>
+                </div>
             </Card>
-        </div>
     );
 }
