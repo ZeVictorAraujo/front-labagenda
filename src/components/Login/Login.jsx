cb3c06cc24df53fb614b92e4d17aa17390a8cb74
Cezar Nogueira <cezartv1221@gmail.com>
2024-10-29 10:48:56 -0300

    Fix: Concluir a Estilização do componente de Login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -6,9 +6,9 @@ import Button from "../Button/Button";
 export default function Login() {
     return (
             <Card size="normal" color="secondary" width="sm">
-                <div className="flex flex-col justify-center gap-16">
+                <div className="flex flex-col justify-center gap-12">
                     {/* Logo do Senai e Sistema */}
-                    <div className="flex flex-col items-center justify-center gap-16">
+                    <div className="flex flex-col items-center justify-center gap-12">
                         <div className="flex itens-center justify-center w-3/4">
                             <Logo type="senai"/>
                         </div>
@@ -17,11 +17,13 @@ export default function Login() {
                         </div>
                     </div>
                     {/* Formulario de Login */}
-                    <form action="POST" className="flex flex-col itens-center justify-center gap-4">
-                        <Input size="normal" type="text" ph="RA" id="ra" name="ra"/>
-                        <Input size="normal" type="text" ph="Senha" id="senha" name="senha"/>
-                        <div className="flex flex-col itens-center justify-center py-4">
-                            <Button size="normal" color="primary" type="text">Entrar</Button>
+                    <form action="POST" className="flex flex-col itens-center justify-center">
+                        <div className="flex flex-col itens-center justify-center py-2 px-16 gap-4">
+                            <Input size="normal" type="text" ph="RA" id="ra" name="ra"/>
+                            <Input size="normal" type="text" ph="Senha" id="senha" name="senha"/>
+                        </div>
+                        <div className="flex flex-col itens-center justify-center py-2 px-16">
+                            <Button size="lg" color="primary" type="text">Entrar</Button>
                         </div>
                     </form>
                 </div>
