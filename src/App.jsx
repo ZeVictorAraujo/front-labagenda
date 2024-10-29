78c4b400ea7ac9ec6778ad2bfbd7df848274c0d2
Cezar Nogueira <cezartv1221@gmail.com>
2024-10-29 20:52:17 -0300

    Fix: Concertar o App

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,8 @@
-import Sidebar from './components/SideBar/SideBar'
+
 
 export default function App() {
   return (
-    <div className="flex h-screen">
-      {/* Sidebar Fixa à Esquerda */}
-      <Sidebar />
-
-      {/* Conteúdo Principal à Direita */}
-      <div className="flex-1 bg-gray-100 p-6 overflow-auto">
-      </div>
+    <div>
     </div>
   );
 }
