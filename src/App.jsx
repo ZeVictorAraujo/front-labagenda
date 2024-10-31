cbd2cdfa6c0313f1ddc960041ba5e833fa6d1292
ZeVictorAraujo <josearaujo.ba@hotmail.com>
2024-10-31 00:12:06 -0300

    Feat: Novas rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage/LoginPage";
+import Home from './pages/Home/Home'
+import Calendar from './pages/Calendar/Calendar'
 
 export default function App() {
   return (
@@ -8,6 +10,8 @@ export default function App() {
       <main>
         <Routes>
           <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/calendar" element={<Calendar />} />
         </Routes>
       </main>
     </Router>
